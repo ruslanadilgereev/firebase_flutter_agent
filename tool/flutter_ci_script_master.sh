13704626eb4fb709f4e695897e56fc554ae7ae80
Eric Windmill <eric@ericwindmill.com>
2025-03-27 12:10:34 -0400

    remove test project from CI

--- tool/flutter_ci_script_master.sh
+++ tool/flutter_ci_script_master.sh
@@ -8,7 +8,7 @@ source "$DIR/flutter_ci_script_shared.sh"
 flutter doctor -v
 
 declare -ar PROJECT_NAMES=(
-    "green_thumb_cloud_next_25"
+    # add projects here
 )
 
 ci_projects "master" "${PROJECT_NAMES[@]}"
