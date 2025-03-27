29819c92f609faceb3643ce9ef18efe8cdb9d3b2
Eric Windmill <eric@ericwindmill.com>
2025-03-27 11:59:08 -0400

    CI initial

---
+++ tool/flutter_ci_script_stable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+DIR="${BASH_SOURCE%/*}"
+source "$DIR/flutter_ci_script_shared.sh"
+
+flutter doctor -v
+
+declare -ar PROJECT_NAMES=(
+    "green_thumb_cloud_next_25"
+)
+
+ci_projects "stable" "${PROJECT_NAMES[@]}"
+
+echo "-- Success --"
