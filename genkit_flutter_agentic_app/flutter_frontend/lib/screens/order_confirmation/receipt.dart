9878a41821d2892b0efccaf98fdedb01b437660e
Khanh Nguyen <KhanhNwin@gmail.com>
2025-04-20 16:41:48 -0700

    remove unused import

--- genkit_flutter_agentic_app/flutter_frontend/lib/screens/order_confirmation/receipt.dart
+++ genkit_flutter_agentic_app/flutter_frontend/lib/screens/order_confirmation/receipt.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_app/models/order_confirmation_model.dart';
 
 import '../../settings/styles.dart';
