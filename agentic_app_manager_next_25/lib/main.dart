670cef7ba049e3ed1f16afdc0260cd43720d2928
Khanh Nguyen <KhanhNwin@gmail.com>
2025-05-19 12:21:49 -0400

    remove unused imports

--- agentic_app_manager_next_25/lib/main.dart
+++ agentic_app_manager_next_25/lib/main.dart
@@ -1,14 +1,6 @@
-import 'package:agentic_app_manager/image_generator.dart';
 import 'package:flutter/material.dart';
-import 'package:feedback/feedback.dart';
-import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'dart:typed_data';
 import 'firebase_options.dart';
-import 'fake_mail_screen/mail_screen.dart';
-import 'agentic_app_manager/app_agent.dart';
-
-import './app_state.dart';
 import './agentic_app_manager/agentic_app_manager_demo.dart';
 
 void main() async {
