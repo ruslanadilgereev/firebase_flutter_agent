9506cadf89e2d0248d50a6faa7fe7cf1bf4a7ee3
adilgereev <74386848+ruslanadilgereev@users.noreply.github.com>
2025-10-19 04:11:08 +0200

    Delete ios_platform_views_io_2025 directory

--- ios_platform_views_io_2025/README.md
+++
@@ -1,4 +0,0 @@
-# ios_platform_views
-
-Demonstrates embedding a SwiftUI view inside a Flutter UI. 
-There are
--- ios_platform_views_io_2025/acrings_native/common/AcringsNativePlugin.swift
+++
@@ -1,9 +0,0 @@
-import Flutter
-import UIKit
-
-public class AcringsNativePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let factory = ACRingsNativeViewFactory(messenger: registrar.messenger())
-    registrar.register(factory, withId:"activity_rings")
-  }
-}
