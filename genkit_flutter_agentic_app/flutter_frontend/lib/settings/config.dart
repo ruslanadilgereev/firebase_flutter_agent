6335ec8f0b080c118770124b89b432342afa111c
adilgereev <74386848+ruslanadilgereev@users.noreply.github.com>
2025-10-19 04:12:23 +0200

    Delete genkit_flutter_agentic_app directory

--- genkit_flutter_agentic_app/flutter_frontend/lib/settings/config.dart
+++
@@ -1,6 +0,0 @@
-// Endpoint to reach Genkit app, can be localhost for development
-// or wherever the live app is hosted ex: Cloud Run.
-// For testing on an Android emulator, you need to specify "10.0.2.2:2222"
-// instead of the default "localhost:2222".
-// See: https://developer.android.com/studio/run/emulator-networking.html
-final String genkitServerEndpoint = 'localhost:2222';
--- genkit_flutter_agentic_app/genkit_backend/src/ai.js
+++
@@ -1,9 +0,0 @@
-// import the Genkit and Google AI plugin libraries
-import { gemini20Flash, googleAI } from '@genkit-ai/googleai';
-import { genkit } from 'genkit';
-
-// configure a Genkit instance
-export const ai = genkit({
-    plugins: [googleAI()],
-    model: gemini20Flash, // set default model
-});
